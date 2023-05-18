a19a033b054ef6d787aa209865dd6b09f52c5e9e
junlie.lin <junlie.lin@feiniu.com>
2023-05-18 17:20:23 +0800

    first commit

--- src/main/java/com.merami.jvm/Jvm.java
+++ src/main/java/com.merami.jvm/Jvm.java
@@ -2,8 +2,9 @@ package com.merami.jvm;
 
 /**
  * @author 作者 merami
- * @date 2023/5/18 16:48
- * @explain 类说明
+ * @version v1.0
+ * <p> 创建时间： 2023/5/18 17:20 </p>
+ * <p> 类说明 </p>
  */
 public class Jvm {
 }
